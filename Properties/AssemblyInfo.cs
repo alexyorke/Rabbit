918e4bf37fceacae55a3bc607a23a42d556cab08
Decagon <decagongithub@gmail.com>
2014-07-19 11:15:23 -0300

    Using .NET 3.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -39,3 +40,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
