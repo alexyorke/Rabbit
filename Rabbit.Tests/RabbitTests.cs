a2cd0d058012331758ce34cbfd82d802aa1d4186
Express50 <press.ex50@gmail.com>
2014-07-22 18:00:45 -0400

    Convert tests to NUnit

--- Rabbit.Tests/RabbitTests.cs
+++ Rabbit.Tests/RabbitTests.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using Rabbit;
 using Rabbit.Auth;
 
 namespace Rabbit.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class RabbitTests
     {
-        [TestMethod]
+        [Test]
         public void NullAuthenticationTest()
         {
             ArgumentNullException expectedException = null;
@@ -26,7 +26,7 @@ namespace Rabbit.Tests
             Assert.IsNotNull(expectedException);
         }
 
-        [TestMethod]
+        [Test]
         public void RegularAuthenticationTest()
         {
             var authResult = Rabbit.GetAuthType("test@email.com", "testpassword");
@@ -34,7 +34,7 @@ namespace Rabbit.Tests
             Assert.AreEqual(AuthType.Regular, authResult);
         }
 
-        [TestMethod]
+        [Test]
         public void FacebookAuthenticationTest()
         {
             var authResult = Rabbit.GetAuthType(null, "TcsL8qdSwvzDqudWGfYEGF5RrBLBSanHW78t5Z87ngKzUDdhCE4Jbtq6Vrwk2vuVS8WW2RYT54hwFxchWywLLvQaUyA2k2fanfAs");
@@ -42,7 +42,7 @@ namespace Rabbit.Tests
             Assert.AreEqual(AuthType.Facebook, authResult);
         }
 
-        [TestMethod]
+        [Test]
         public void KongregateAuthenticationTest()
         {
             var authResult = Rabbit.GetAuthType("123456", "969ad76abf19b5c3e5917321e659abe6d8d6f3aba73e5158863c3b4159c00366");
@@ -50,7 +50,7 @@ namespace Rabbit.Tests
             Assert.AreEqual(AuthType.Kongregate, authResult);
         }
 
-        [TestMethod]
+        [Test]
         public void ArmorGamesAuthenticationTest()
         {
             var authResult = Rabbit.GetAuthType("ee6ab941d09050400c4f916dbb47aad8", "23b9a5088c3c940f82945b6f3df80abc");
