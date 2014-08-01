f197ab464041bc5f956a1b614272d8df50756f13
Express50 <press.ex50@gmail.com>
2014-08-01 12:09:59 -0400

    Move IdParser tests to IdParsersTests class
    Also add InvalidRoomIDTest

---
+++ Rabbit.Tests/IdParserTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Rabbit.Tests
+{
+    [TestFixture]
+    public class IdParserTests
+    {
+        [Test]
+        public void InvalidRoomIDTest()
+        {
+            FormatException expectedException = null;
+
+            try
+            {
+                IdParser.Parse("test");
+            }
+
+            catch (FormatException ex)
+            {
+                expectedException = ex;
+            }
+
+            Assert.AreEqual(IdParser.IsValidStrictRoomId("test"), false);
+            Assert.IsNotNull(expectedException);
+        }
+
+        [Test]
+        public void ValidRoomIDTest()
+        {
+            Assert.AreEqual(IdParser.IsValidStrictRoomId("PWmSQ3dUeya0I"), true);
+            Assert.AreEqual(IdParser.Parse("PWmSQ3dUeya0I"), "PWmSQ3dUeya0I");
+        }
+    }
+}
