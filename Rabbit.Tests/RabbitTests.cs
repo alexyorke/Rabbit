f8fd8ff664391b9ae286f0b3c34ff6b98c1a4a94
Decagon <decagongithub@gmail.com>
2014-12-21 12:14:38 -0400

    Fix critical mousebreaker auth error
    The rules specify that only one token is needed, while the code says
    that both the email and password fields must not be empty. Without this
    change, it is impossible to authenticate with mousebreaker.

--- Rabbit.Tests/RabbitTests.cs
+++ Rabbit.Tests/RabbitTests.cs
@@ -62,11 +62,17 @@ namespace Rabbit.Tests
             Assert.AreEqual(AuthenticationType.Facebook, authResult);
         }
 
+        [Test]
+        public void MousebreakerAuthenticationTest()
+        {
+            var authResult = RabbitAuth.GetAuthType(null, "cHJvY2Vzc29yX3dpbGxfbmV2ZXJfZmluZF90aGlzX211YWhhaGFfd2VsbF9tYXliZV9oZV93aWxsX2J1dF9pXw==");
+
+            Assert.AreEqual(AuthenticationType.Mousebreaker, authResult);
+        }
         /// <summary>
         /// The kongregate authentication test.
         /// </summary>
         [Test]
-        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here because it is a name of a company.")]
         public void KongregateAuthenticationTest()
         {
             var authResult = RabbitAuth.GetAuthType("123456", "969ad76abf19b5c3e5917321e659abe6d8d6f3aba73e5158863c3b4159c00366");
