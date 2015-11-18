1a4d0a382322abc3c7332b0b463a163e0ec002b3
Decagon <decagongithub@gmail.com>
2015-11-18 07:26:57 -0400

    Remove unused tests

--- Rabbit.Tests/IdParserTests.cs
+++ Rabbit.Tests/IdParserTests.cs
@@ -6,50 +6,23 @@ namespace Rabbit.Tests
     [TestFixture]
     public class IdParserTests
     {
-        [Test]
-        public void InvalidRoomIdTest()
-        {
-            FormatException expectedException = null;
-
-            try
-            {
-                IdParser.TryParse("test");
-            }
-
-            catch (FormatException ex)
-            {
-                expectedException = ex;
-            }
-
-            Assert.AreEqual(IdParser.IsValidStrictRoomId("test"), false);
-            Assert.IsNotNull(expectedException);
-        }
 
         [Test]
         public void RoomIdTooLongTest()
         {
-            FormatException expectedException = null;
+            Exception expectedException = null;
 
             try
             {
-                IdParser.TryParse("PWthisroomnameiswaytoolongtoprocessefficientlyomgz12");
+                string worldid;
+                IdParser.TryParse("http://domain.com/PWthisroomnameiswaytoolongtoprocessefficientlyomgz12", out worldid);
             }
-            catch (FormatException ex)
+            catch (Exception ex)
             {
                 expectedException = ex;
             }
 
             Assert.IsNotNull(expectedException);
-        }
-
-        [Test]
-        public void ValidRoomIdTest()
-        {
-            Assert.AreEqual(IdParser.IsValidStrictRoomId("PWtest12345"), true);
-            Assert.AreEqual(IdParser.TryParse("PWtest12345"), "PWtest12345");
-            Assert.AreEqual(IdParser.TryParse("OWopenroom"), "OWopenroom");
-            Assert.AreEqual(IdParser.TryParse("http://everybodyedits.com/games/PWsomething"), "PWsomething");
-
         }
     }
 }
